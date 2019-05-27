b19aec179a70b4efb8968a479ba2d28aac7c1597
youb1996 <227662@supinfo.com>
2019-05-26 18:08:47 -0700

    vue init

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Blocks from './Blocks';
+
+class App extends Component {
+
+    state = { walletInfo: {} };
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info')
+            .then(res => res.json())
+            .then(json => this.setState({ walletInfo: json }));
+    }
+
+    render() {
+        const { address, balance } = this.state.walletInfo;
+
+        return (
+            <div>
+                <div>welcome to the blockchen ...</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
+                <br />
+                <Blocks/>
+            </div>
+        );
+    }
+}
+
+export default App;
